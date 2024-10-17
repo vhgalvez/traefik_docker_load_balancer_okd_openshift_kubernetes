268fb2796af55c3c2b7f9be043dcc1faa7a84485
Victor Galvez <vhgalvez@gmail.com>
2024-10-17 03:33:33 +0200

    update

--- README.md
+++ README.md
@@ -4,4 +4,33 @@ traefik_docker_load_balancer_okd_openshift_kubernetes
 
 ## Description
 
+
+
+
+
+## Installation
+
+sudo mkdir -p /etc/traefik/custom-ca
+cd /etc/traefik/custom-ca
+
+# Generar la clave privada de la CA
+sudo openssl genrsa -out myCA.key 4096
+
+# Crear el certificado de la CA
+sudo openssl req -x509 -new -nodes -key myCA.key -sha256 -days 1024 -out myCA.pem -subj "/CN=MyCustomCA"
+
+
+
+```bash
 sudo cp -r /$PWD/*  /etc/traefik/
+```
+
+
+
+## Usage
+
+```bash
+sudo chmod +x ./openssl.sh
+sudo ./openssl.sh
+
+
--- openssl.sh
+++ openssl.sh
@@ -6,10 +6,17 @@ sudo mkdir -p /etc/traefik/custom-ca
 sudo chmod 700 /etc/traefik/custom-ca
 sudo chmod 700 /etc/traefik/ssl
 
-# Verificar que los archivos CA existan
+# Verificar si los archivos CA ya existen; si no, generarlos
 if [[ ! -f /etc/traefik/custom-ca/myCA.pem || ! -f /etc/traefik/custom-ca/myCA.key ]]; then
-  echo "Los archivos myCA.pem o myCA.key no existen en /etc/traefik/custom-ca. Por favor, verifica."
-  exit 1
+    echo "Generando myCA.pem y myCA.key en /etc/traefik/custom-ca..."
+    
+    # Generar la clave privada de la CA
+    sudo openssl genrsa -out /etc/traefik/custom-ca/myCA.key 4096
+    
+    # Crear el certificado de la CA
+    sudo openssl req -x509 -new -nodes -key /etc/traefik/custom-ca/myCA.key -sha256 -days 1024 -out /etc/traefik/custom-ca/myCA.pem -subj "/CN=MyCustomCA"
+else
+    echo "Los archivos myCA.pem y myCA.key ya existen en /etc/traefik/custom-ca. Saltando generación de CA."
 fi
 
 # Copiar el CA a /etc/myCA
@@ -37,14 +44,16 @@ DNS.1 = api.local.cefaslocalserver.com
 DNS.2 = api-int.local.cefaslocalserver.com
 EOF
 
-# Generar clave privada
-echo "Generando clave privada..."
+# Generar clave privada para el certificado del servidor
+echo "Generando clave privada para cefaslocalserver.com..."
 openssl genrsa -out /etc/traefik/ssl/cefaslocalserver.com.key 4096
 
 # Crear el CSR (Certificate Signing Request)
-echo "Generando CSR..."
+echo "Generando CSR para cefaslocalserver.com..."
 openssl req -new -key /etc/traefik/ssl/cefaslocalserver.com.key -out /etc/traefik/ssl/cefaslocalserver.com.csr -config /etc/traefik/ssl/extfile.cnf
 
 # Firmar el certificado usando el CA personalizado
 echo "Firmando el certificado con el CA personalizado..."
 openssl x509 -req -in /etc/traefik/ssl/cefaslocalserver.com.csr -CA /etc/traefik/custom-ca/myCA.pem -CAkey /etc/traefik/custom-ca/myCA.key -CAcreateserial -out /etc/traefik/ssl/cefaslocalserver.com.crt -days 365 -extensions v3_req -extfile /etc/traefik/ssl/extfile.cnf
+
+echo "Certificado generado correctamente en /etc/traefik/ssl/cefaslocalserver.com.crt"
